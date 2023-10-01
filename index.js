59c1aa06e145f4d2040d69cf46269196f23aafea
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-10-01 15:23:39 -0400

    Added modules and morgan log filing

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+
 const app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
