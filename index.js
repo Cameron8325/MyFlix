8b77b844f88a1003fb4a4be4215a9c2ea81d3cdf
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-10-01 15:59:47 -0400

    Reformatted

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express'),
-    morgan = require('morgan'),
-    fs = require('fs'),
+const express = require('express');
+const app = express();
+    morgan = require('morgan');
+    fs = require('fs');
     path = require('path');
 
-const app = express();
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
 
