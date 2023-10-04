682c8649ca412006126edf17708fbf5c65b27028
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-10-04 16:35:02 -0400

    Added Express code and Endpoints

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.static('public'));
 
+// Endpoint to return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
     const top10Movies = [
         {
@@ -75,6 +76,54 @@ app.get('/movies', (req, res) => {
         }
     ];
     res.json(top10Movies);
+});
+
+// Endpoint to return data about a single movie by title
+app.get('/movies/:title', (req, res) => {
+    const movieTitle = req.params.title;
+    res.send(`Return data about the movie with title: ${movieTitle}`);
+});
+
+// Endpoint to return data about a genre by name
+app.get('/genres/:name', (req, res) => {
+    const genreName = req.params.name;
+    res.send(`Return data about the genre with name: ${genreName}`);
+});
+
+// Endpoint to return data about a director by name
+app.get('/directors/:name', (req, res) => {
+    const directorName = req.params.name;
+    res.send(`Return data about the director with name: ${directorName}`);
+});
+
+// Endpoint to allow new users to register
+app.post('/users/register', (req, res) => {
+    res.send('User registration successful');
+});
+
+// Endpoint to allow users to update their user info
+app.put('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`Update user info for user with ID: ${userId}`);
+});
+
+// Endpoint to allow users to add a movie to their list of favorites
+app.post('/users/:userId/favorites', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`Movie added to favorites for user with ID: ${userId}`);
+});
+
+// Endpoint to allow users to remove a movie from their list of favorites
+app.delete('/users/:userId/favorites/:movieId', (req, res) => {
+    const userId = req.params.userId;
+    const movieId = req.params.movieId;
+    res.send(`Movie removed from favorites for user with ID: ${userId}, Movie ID: ${movieId}`);
+});
+
+// Endpoint to allow existing users to deregister
+app.delete('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`User with ID ${userId} has been deregistered`);
 });
 
 app.get('/', (req, res) => {
