9d8ca038ccdb4121089e473999c1b42d459a7c4a
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-10-24 11:34:23 -0400

    Removed in memory list, added async movie req

--- index.js
+++ index.js
@@ -23,71 +23,10 @@ app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.static('public'));
 
 // Endpoint to return a list of ALL movies to the user
-app.get('/movies', (req, res) => {
-    const top10Movies = [
-        {
-            title: 'The Shawshank Redemption',
-            director: 'Frank Darabont',
-            year: 1994,
-            rating: 9.3,
-        },
-        {
-            title: 'The Godfather',
-            director: 'Francis Ford Coppola',
-            year: 1972,
-            rating: 9.2,
-        },
-        {
-            title: 'The Dark Knight',
-            director: 'Christopher Nolan',
-            year: 2008,
-            rating: 9.0,
-        },
-        {
-            title: 'Pulp Fiction',
-            director: 'Quentin Tarantino',
-            year: 1994,
-            rating: 8.9,
-        },
-        {
-            title: 'Schindler\'s List',
-            director: 'Steven Spielberg',
-            year: 1993,
-            rating: 8.9,
-        },
-        {
-            title: 'Forrest Gump',
-            director: 'Robert Zemeckis',
-            year: 1994,
-            rating: 8.8,
-        },
-        {
-            title: 'The Matrix',
-            director: 'Lana Wachowski, Lilly Wachowski',
-            year: 1999,
-            rating: 8.7,
-        },
-        {
-            title: 'Inception',
-            director: 'Christopher Nolan',
-            year: 2010,
-            rating: 8.7,
-        },
-        {
-            title: 'Goodfellas',
-            director: 'Martin Scorsese',
-            year: 1990,
-            rating: 8.7,
-        },
-        {
-            title: 'Fight Club',
-            director: 'David Fincher',
-            year: 1999,
-            rating: 8.8,
-        }
-    ];
-    res.json(top10Movies);
+app.get('/movies', async (req, res) => {
+    Movies.find().then(Movies => res.json(Movies));
 });
+
 
 // Endpoint to return data about a single movie by title
 app.get('/movies/:title', (req, res) => {
