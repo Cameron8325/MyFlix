0a5d1205b669b1c5e04492122e4092992166a056
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-10-25 10:04:29 -0400

    Cleaned up debugging code

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect('mongodb://127.0.0.1:27017/cfDB');
-console.log(mongoose.connection.readyState);
 
 const express = require('express');
 const app = express();
@@ -26,7 +24,6 @@ app.use(express.static('public'));
 
 // Endpoint to return a list of ALL movies to the user
 app.get('/movies', async (req, res) => {
-  console.log(mongoose.connection.readyState);
     Movies.find().then(Movies => res.json(Movies));
 });
 
